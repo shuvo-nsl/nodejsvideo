24acc66aa8d9eaa4422bad183da561a020c30ba4
shuvo-nsl <107601396+shuvo-nsl@users.noreply.github.com>
2024-05-21 09:21:23 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,66 +1,28 @@
-require('dotenv').config();
-const express = require("express");
-const http = require("http");
-const app = express();
-const server = http.createServer(app);
-const socket = require("socket.io");
-const io = socket(server);
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+})
 
-const users = {};
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
 
-const socketToRoom = {};
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
 
-io.on('connection', socket => {
-    socket.on("join room", roomID => {
-        if (users[roomID]) {
-            const length = users[roomID].length;
-            if (length === 10) {
-                socket.emit("room full");
-                return;
-            }
-            users[roomID].push(socket.id);
-        } else {
-            users[roomID] = [socket.id];
-        }
-        socketToRoom[socket.id] = roomID;
-        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
 
-        socket.emit("all users", usersInThisRoom);
-    });
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
 
-    socket.on("sending signal", payload => {
-        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-    });
-
-    socket.on("returning signal", payload => {
-        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-    });
-
-    // socket.on('disconnect', () => {
-    //     const roomID = socketToRoom[socket.id];
-    //     let room = users[roomID];
-    //     if (room) {
-    //         room = room.filter(id => id !== socket.id);
-    //         users[roomID] = room;
-    //     }
-    // });
-    socket.on('disconnect', () => {
-        const roomID = socketToRoom[socket.id];
-        let room = users[roomID];
-
-        if (room) {
-            room = room.filter(id => id !== socket.id);
-            users[roomID] = room;
-            socket.broadcast.to(roomID).emit("user left", socket.id);
-            if (room.length === 0) {
-                delete users[roomID];
-            }
-        }
-        delete socketToRoom[socket.id];
-    });
-
-});
-
-server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
-
-
+server.listen(5000, () => console.log("server is running on port 5000"))
